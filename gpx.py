bef49ae1ffb8e5946d01c6d3d569f821f2907c1b
Peter McColgan <pmccolga@xilinx.com>
2020-08-02 12:33:06 +0100

    calculate curvature of gpx track, to be used to cap cornering speeds

--- gpx.py
+++ gpx.py
@@ -1,12 +1,13 @@
 from scipy.interpolate import interp1d
 from scipy.misc import derivative
+from numpy import unwrap
 from functools import partial
 import gpxpy
 from gpxpy.gpx import mod_geo
 import math
 
-#with open('sample.gpx','r') as gpx_file:
-with open('minisample.gpx','r') as gpx_file:
+with open('sample.gpx','r') as gpx_file:
+#with open('minisample.gpx','r') as gpx_file:
     points = gpxpy.parse(gpx_file).walk()
 points = list(map(lambda t: t[0],points))
 
@@ -27,6 +28,9 @@ for a,b in segs:
         elev.append(b.elevation)
         course.append(math.radians(a.course_between(b)))
 
+course = unwrap(course)
+
 f_elev = interp1d(x,elev,bounds_error=False,fill_value='extrapolate',kind='cubic',assume_sorted=True)
 f_grad = partial(derivative,f_elev)
 f_course = interp1d(x[:-1],course,bounds_error=False,fill_value='extrapolate',kind='previous',assume_sorted=True)
+f_curve = partial(derivative,f_course)
